99d6972c9aad95dea4ead32b8be08187c788f135
Carlos E. Araujo <110763014+carlos-e-araujo@users.noreply.github.com>
2024-12-20 20:39:55 -0300

    feature add celula model;

--- src/com/jogodedamas/celula/Celula.java
+++ src/com/jogodedamas/celula/Celula.java
@@ -1,4 +1,26 @@
 package com.jogodedamas.celula;
 
+import com.jogodedamas.cor.Cor;
+import com.jogodedamas.peca.Peca;
+
 public class Celula {
+    private final Cor COR;
+    private Peca peca;
+
+    public Celula(Cor cor) {
+        COR = cor;
+        peca = null;
+    }
+
+    public Cor getCor() {
+        return COR;
+    }
+
+    public Peca getPeca() {
+        return peca;
+    }
+
+    public void setPeca(Peca peca) {
+        this.peca = peca;
+    }
 }
