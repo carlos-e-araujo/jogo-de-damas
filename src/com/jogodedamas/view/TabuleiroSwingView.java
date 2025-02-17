edc44fd65cb6135856f9637c7ea0fc45d1185a28
carlos-e-araujo <110763014+carlos-e-araujo@users.noreply.github.com>
2025-02-17 09:54:44 -0300

    refactor: replace CasaJButton with JButton and update board size in TabuleiroSwingView

--- src/com/jogodedamas/view/TabuleiroSwingView.java
+++ src/com/jogodedamas/view/TabuleiroSwingView.java
@@ -4,26 +4,27 @@ import com.jogodedamas.model.Comum;
 import com.jogodedamas.model.Dama;
 import com.jogodedamas.model.Tabuleiro;
 import com.jogodedamas.utils.Cor;
-import com.jogodedamas.utils.Posicao;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class TabuleiroSwingView extends JFrame {
-    private final CasaJButton[][] casaJButtons;
+    private final JButton[][] casaJButtons;
 
     public TabuleiroSwingView() {
         setTitle("Jogo de Damas");
-        setSize(600, 600);
+        setSize(800, 800);
+        setMinimumSize(new Dimension(800, 800));
+        setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
         JPanel tabuleiroPanel = new JPanel(new GridLayout(8, 8));
-        this.casaJButtons = new CasaJButton[8][8];
+        this.casaJButtons = new JButton[8][8];
 
         for (int linha = 0; linha < 8; linha++) {
             for (int coluna = 0; coluna < 8; coluna++) {
-                CasaJButton casaJButton = new CasaJButton(linha, coluna);
+                JButton casaJButton = new JButton();
 
                 this.casaJButtons[linha][coluna] = casaJButton;
                 this.casaJButtons[linha][coluna].setFont(new Font("Arial", Font.PLAIN, 50));
@@ -82,19 +83,5 @@ public class TabuleiroSwingView extends JFrame {
         }
 
         this.atualizarPosicoes(tabuleiro);
-    }
-}
-
-class CasaJButton extends JButton {
-    private final int linha;
-    private final int coluna;
-
-    public CasaJButton(int linha, int coluna) {
-        this.linha = linha;
-        this.coluna = coluna;
-    }
-
-    public Posicao getPosicao() {
-        return Posicao.toPosicao(this.linha, this.coluna);
     }
 }
